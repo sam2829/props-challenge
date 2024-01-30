44566be6c54e8ca6dfbd2ee826a479e2084ab47f
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-01-30 08:55:28 +0000

    Add navbarsimple challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar"
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
       {/*Add your components here*/}
       <Sidebar />
+      <NavBarSimple />
     </div>
   );
 }
---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,35 @@
+import React from "react";
+import css from "./css/NavBarSimple.module.css"
+
+
+class NavBarSimple extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "Hello, guest!",
+            buttonText: "Log in",
+        }
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            return {
+                message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+                buttonText: prevState.buttonText === "Log in" ? "Log out" : "Log in",
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className={css.NavBar}>
+                <h1></h1>
+                <span>{this.state.message}</span>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default NavBarSimple;
