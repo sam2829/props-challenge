9db3fdb9274751d42df66a4f7e4b8d8af7e8d5ca
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-02 12:40:36 +0000

    Add content hooks challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import css from './App.module.css';
 import Sidebar from "./components/Sidebar"
 //import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
+import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       {/*Add your components here*/}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      <ContentHooks />
 
       
     </div>
---
+++ my-app/src/components/ContentHooks.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react'
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
+import Loader from "./Loader"
+
+function ContentHooks() {
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [fetchedPosts, setFetchedPosts] = useState([]);
+
+    useEffect(()=>{
+        setTimeout(() => {
+            setIsLoaded(true);
+            setFetchedPosts(savedPosts);
+        }, 2000)
+    }, []);
+
+    const handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+        
+        setFetchedPosts(filteredPosts)
+    }
+
+    return (
+        <div className={css.Content}>
+            <div className={css.TitleBar}>
+                <h1>My Photos</h1>
+                <form>
+                    <label htmlFor="id-searchInput">Search</label>
+                    <input
+                        id='id-searchInput'
+                        name="searchInput"
+                        placeholder='By Author'
+                        type='search'
+                        onChange={(e)=>{handleChange(e)}}
+                    />
+                    <h4>posts found {fetchedPosts.length}</h4>
+                </form>
+            </div>
+
+            <div className={css.SearchResults}>
+                {
+                    isLoaded ?
+                    <PostItem savedPosts={fetchedPosts} />
+                    : <Loader />
+                }
+            </div>
+        </div>
+    )
+}
+
+export default ContentHooks
