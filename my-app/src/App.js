bc7fc8a7360eafe43e8201453ac75d41ba5d5bbf
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-03 11:17:03 +0000

    Converting class component to using and api challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Sidebar from "./components/Sidebar"
 //import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import ContentHooks from './components/ContentHooks';
+import ContentAPI from './components/ContentAPI';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/*Add your components here*/}
       <NavBarForm />
       <Sidebar />
-      <ContentHooks />
+      {/*<ContentHooks />*/}
+      <ContentAPI />
 
       
     </div>
